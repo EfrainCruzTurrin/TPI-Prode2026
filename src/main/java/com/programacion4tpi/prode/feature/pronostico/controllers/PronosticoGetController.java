2d72b6f163f1dc0277202d1692c373e4bcb26040
RamiroHH <ramiroh.dev@gmail.com>
2026-06-21 21:41:44 -0300

    Update PronosticoGetController.java

--- src/main/java/com/programacion4tpi/prode/feature/pronostico/controllers/PronosticoGetController.java
+++ src/main/java/com/programacion4tpi/prode/feature/pronostico/controllers/PronosticoGetController.java
@@ -1,14 +1,16 @@
 package com.programacion4tpi.prode.feature.pronostico.controllers;
 
+import com.programacion4tpi.prode.feature.pronostico.dtos.resp.PronosticoGetResponseDto;
 import com.programacion4tpi.prode.feature.pronostico.dtos.resp.PronosticoResponseDto;
+import com.programacion4tpi.prode.feature.pronostico.dtos.resp.RankingResponseDto;
 import com.programacion4tpi.prode.feature.pronostico.services.interfaces.IPronosticoGetService;
+import com.programacion4tpi.prode.feature.usuario.models.Usuario;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pronostico")
@@ -20,5 +22,22 @@ public class PronosticoGetController {
     @GetMapping
     public ResponseEntity<List<PronosticoResponseDto>> listarPronosticos() {
         return ResponseEntity.ok(pronosticoGetService.listarPronosticos());
+    }
+
+    @GetMapping("/ranking")
+    public ResponseEntity<List<RankingResponseDto>> getRanking() {
+        return ResponseEntity.ok(pronosticoGetService.getRanking());
+    }
+
+    @GetMapping("/partido/{partidoId}/terceros")
+    public ResponseEntity<List<PronosticoGetResponseDto>> getPronosticosDeOtros(
+            @PathVariable Long partidoId,
+            Authentication authentication) {
+
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+        List<PronosticoGetResponseDto> resultado =
+                pronosticoGetService.getPronosticosDeOtroUsuario(partidoId, usuario.getId());
+
+        return ResponseEntity.ok(resultado);
     }
 }
